2ffff1c31985079d8b7f5e85a68212cdece70c9d
Yurii Orlyk <orlyk.rollun@gmail.com>
2021-05-05 15:31:46 +0300

    deleted docs

--- test/Unit/Openapi/OpenapiTest.php
+++ test/Unit/Openapi/OpenapiTest.php
@@ -23,17 +23,18 @@ class OpenapiTest extends TestCase
     {
         global $container;
         self::$container = $container;
-        //self::$pid = exec('php -S localhost:8000 1>/dev/null & echo $!');
+        self::$pid = exec('php -S localhost:8000 1>/dev/null & echo $!');
     }
 
     public static function tearDownAfterClass()
     {
-        //exec('kill -9 ' . self::$pid);
-        //exec('rm -rf src/Test');
-        //unlink('config/autoload/test_v1_0_1_path_handler.global.php');
+        exec('kill -9 ' . self::$pid);
+        exec('rm -rf src/Test');
+        exec('rm -rf public/openapi/docs/Test');
+        unlink('config/autoload/test_v1_0_1_path_handler.global.php');
     }
 
-    /*public function testGenerateServer()
+    public function testGenerateServer()
     {
         $command = 'php bin/openapi-generator generate:server --manifest=test.yaml';
         exec($command, $outputs);
@@ -43,12 +44,21 @@ class OpenapiTest extends TestCase
         $this->assertFileExists('src/Test/src/OpenAPI/V1_0_1/DTO/Test.php');
         $this->assertFileExists('src/Test/src/OpenAPI/V1_0_1/Server/Handler/Test.php');
         $this->assertFileExists('src/Test/src/OpenAPI/V1_0_1/Server/Rest/Test.php');
-    }*/
+
+        // TODO
+        $content = file_get_contents('src/Test/src/OpenAPI/V1_0_1/Server/Rest/Test.php');
+        $content = str_replace(
+            "'Name of service which implements OpenApi logic'",
+            '\\' . ControllerObject::class . '::class',
+            $content
+        );
+        file_put_contents('src/Test/src/OpenAPI/V1_0_1/Server/Rest/Test.php', $content);
+    }
 
     /**
      * @dependss testGenerateServer
      */
-    /*public function testGenerateClient()
+    public function testGenerateClient()
     {
         $command = 'php bin/openapi-generator generate:client --manifest=test.yaml --debug=true';
         exec($command);
@@ -58,31 +68,22 @@ class OpenapiTest extends TestCase
         $this->assertFileExists('src/Test/src/OpenAPI/V1_0_1/Client/Api/TestApi.php');
         $this->assertFileExists('src/Test/src/OpenAPI/V1_0_1/Client/Rest/Test.php');
         $this->assertFileExists('src/Test/src/OpenAPI/V1_0_1/Client/Configuration.php');
-    }*/
+    }
 
     /**
      * @dependss testGenerateClient
      */
     public function testInteraction()
     {
-
-
-        $config = self::$container->get('config');
+        /*$config = self::$container->get('config');
         $config['dependencies']['invokables'][ControllerObject::class] = ControllerObject::class;
         $container = new ServiceManager();
         $container->configure($config['dependencies']);
         $container->setService('config', $config);
 
-        //$controllerObject = $container->get(ControllerObject::class);
         InsideConstruct::setContainer($container);
-        $server = new ServerRest(new ControllerObject());
 
-        $server = new class() extends Test {
-            const CONTROLLER_OBJECT = ControllerObject::class;
-        };
-        InsideConstruct::setContainer(self::$container);
-
-        $container->get(get_class($testHandler));
+        InsideConstruct::setContainer(self::$container);*/
 
         $clientClass = '\\Test\\OpenAPI\\V1_0_1\\Client\\Rest\\Test';
         $dtoClass = '\Test\\OpenAPI\\V1_0_1\\DTO\\Test';
@@ -92,10 +93,10 @@ class OpenapiTest extends TestCase
         $test->name = 'Test';*/
 
         $client = self::$container->get($clientClass);
-        /*$response = $client->post([
+        $response = $client->post([
             'id' => '12345',
             'name' => 'Test',
-        ]);*/
+        ]);
 
     }
 }
